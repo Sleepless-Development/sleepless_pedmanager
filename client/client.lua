6206cedf21d6d01249c604b9564d5faf80fe45f8
Demigod <austinjobs2shop@gmail.com>
2023-07-31 20:22:35 -0700

    feat(client): support callback functions for ped spawn/despawn

--- client/client.lua
+++ client/client.lua
@@ -1,36 +1,50 @@
 local peds = {
     {
-        model = "s_m_y_construct_01",
+        model = "s_f_y_airhostess_01",
         coords = vec4(3.1629, -1309.7675, 30.1646, 7.2837),
         renderDistance = 30.0,
-        scenario = "WORLD_HUMAN_CLIPBOARD",
+        -- scenario = "WORLD_HUMAN_CLIPBOARD", --optionally use a scenario or an animation.
         animation = {
-            dict = "dict_name",
-            anim = "anim_name"
+            dict = "amb@code_human_in_bus_passenger_idles@female@tablet@idle_a",
+            anim = "idle_a",
+            flag = 63
         },
-        targetOptions = {
+        prop = {
+            propModel = "prop_cs_tablet",
+            bone = 28422,
+            rotation = vec3(0.0, 0.0, 0.03),
+            offset = vec3(0.0, 0.0, 0.03),
+        },
+        targetOptions = { --whatever normal options for ox_target
             {
                 icon = 'fas fa-money-bill-alt',
                 label = 'something',
                 serverEvent = "some event"
             },
         },
+        onSpawn = function(ped, pedData)
+            print(("ped handle is %s"):format(ped))
+            print(json.encode(pedData, { indent = true }))
+        end,
+        onDespawn = function(ped, pedData)
+            print(("ped handle is %s"):format(ped))
+            print(json.encode(pedData, { indent = true }))
+        end
     },
 }
 
 function spawnped(index)
     if not peds[index].ped then
+        local pedModel = peds[index].model
 
-        local model = peds[index].model
-
-        lib.requestModel(model, 500)
+        lib.requestModel(pedModel, 500)
 
         local coords = peds[index].coords
-        peds[index].ped = CreatePed(5, model, coords.x,coords.y,coords.z - 1.0 ,coords.w, false, false)
+        peds[index].ped = CreatePed(5, pedModel, coords.x, coords.y, coords.z - 1.0, coords.w, false, false)
 
         local currentPed = peds[index].ped
         SetPedDefaultComponentVariation(currentPed)
-        FreezeEntityPosition(currentPed,true)
+        FreezeEntityPosition(currentPed, true)
         SetEntityInvincible(currentPed, true)
         SetBlockingOfNonTemporaryEvents(currentPed, true)
         SetPedFleeAttributes(currentPed, 0, 0)
@@ -39,7 +53,8 @@ function spawnped(index)
         if peds[index].animation then
             ClearPedTasksImmediately(currentPed)
             lib.requestAnimDict(peds[index].animation.dict, 500)
-            TaskPlayAnim(currentPed, peds[index].animation.dict, peds[index].animation.anim, 3.0, -8, -1, peds[index].animation.flag, 0, 0, 0, 0 )
+            TaskPlayAnim(currentPed, peds[index].animation.dict, peds[index].animation.anim, 3.0, -8, -1,
+                peds[index].animation.flag, 0, false, false, false)
         end
 
         if peds[index].scenario then
@@ -48,15 +63,34 @@ function spawnped(index)
         end
 
         if peds[index].prop then
-            lib.requestModel(peds[index].prop.propHash, 500)
-            peds[index].prop.entity = CreateObject(peds[index].prop.propHash, GetEntityCoords(peds[index].prop.entity), 0, 0, 1)
-            AttachEntityToEntity(peds[index].prop.entity, currentPed, GetPedBoneIndex(currentPed, 28422), peds[index].prop.rotation.x, peds[index].prop.rotation.y, peds[index].prop.rotation.z, peds[index].prop.offset.x, peds[index].prop.offset.y, peds[index].prop.offset.z, 1, 1, 0, 1, 0, 1)
+            local propData = peds[index].prop
+            local propModel = joaat(propData.propModel)
+            lib.requestModel(propModel, 500)
+            propData.entity = CreateObject(propData.propHash, GetEntityCoords(propData.entity), 0, 0, 1, false, false)
+            AttachEntityToEntity(
+                propData.entity,
+                currentPed,
+                GetPedBoneIndex(currentPed, 28422),
+                propData.rotation.x,
+                propData.rotation.y,
+                propData.rotation.z,
+                propData.offset.x,
+                propData.offset.y,
+                propData.offset.z,
+                true, true, false, true, 0, true
+            )
         end
 
+        if peds[index].onSpawn then
+            peds[index].onSpawn(peds[index].ped, peds[index])
+        end
     end
-  end
+end
 
 function dismissped(index)
+    if peds[index].onDespawn then
+        peds[index].onDespawn(peds[index].ped, peds[index])
+    end
 
     if peds[index].prop.entity then
         DeletePed(peds[index].prop.entity)
@@ -68,7 +102,6 @@ function dismissped(index)
     DeletePed(peds[index].ped)
     peds[index].ped = nil
 end
-
 
 CreateThread(function()
     for i = 1, #peds do

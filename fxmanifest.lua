3bc19a892f9bdad9fd305139c9a3d53cffa3b8b8
Austin Dunn <austindunn.dev@gmail.com>
2024-03-07 11:35:49 -0800

    chore(manifest): remove web

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,13 +4,10 @@ lua54 'yes'
 
 version '2.0.1'
 
-ui_page 'web/build/index.html'
+files {
+    "data.lua"
+}
 
 shared_script "@ox_lib/init.lua"
 client_script "client/**/*"
 server_script "server/**/*"
-
-files {
-	'web/build/index.html',
-	'web/build/**/*',
-}
